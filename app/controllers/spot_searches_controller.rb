33af00030a501394d76651eb1495e655ed10d9c4
sandy guillot <sandyguillot@gmail.com>
2019-06-04 15:29:00 +0200

    adding changes in adresses and spot_searches controllers

--- app/controllers/spot_searches_controller.rb
+++ app/controllers/spot_searches_controller.rb
@@ -3,6 +3,7 @@ class SpotSearchesController < ApplicationController
     @spot_search = SpotSearch.find(params[:id])
     @address = Address.new
     session[:spot_search_id] = @spot_search.id
+    @myaddresses = Address.where(user: current_user)
   end
 
   def create
