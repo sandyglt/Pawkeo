c95ac9ebb9b487e745b8c2900cc06f64071f504c
Frederic Laffont <frederic_laffont@msn.com>
2019-06-04 19:16:18 +0200

    remove redirect action home pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :designhome]
 
   def home
-    redirect_to user_session_path
   end
 
   def designhome
