487b2b03b59b6e7c2f5bb983d349b2f55c5dfb9f
Myriam Chahlafi <myriam.chah@gmail.com>
2019-06-04 18:53:58 +0200

    home login card v1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :designhome]
 
   def home
+    redirect_to user_session_path
   end
 
   def designhome
