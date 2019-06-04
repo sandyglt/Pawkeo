e59f2aa5917591692752476d89cec90565a7989d
sandy guillot <sandyguillot@gmail.com>
2019-06-04 15:12:23 +0200

    changes in progress"

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,17 @@
 class AddressesController < ApplicationController
-  def new
+  def create
+    @address = current_user.addresses.build(address_params)
+    @spot_search = SpotSearch.find(session[:spot_search_id])
+    if @address.save
+      redirect_to spot_search_path(@spot_search)
+    else
+      render :new
+    end
   end
 
-  def create
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :user_id)
   end
 end
--- app/controllers/spot_searches_controller.rb
+++ app/controllers/spot_searches_controller.rb
@@ -1,5 +1,8 @@
 class SpotSearchesController < ApplicationController
   def show
+    @spot_search = SpotSearch.find(params[:id])
+    @address = Address.new
+    session[:spot_search_id] = @spot_search.id
   end
 
   def create
