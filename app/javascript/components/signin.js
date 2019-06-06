c55cac074acba98b5759d72449d22486fb03ba76
Myriam Chahlafi <myriam.chah@gmail.com>
2019-06-06 12:21:31 +0200

    add form display when click on start

---
+++ app/javascript/components/signin.js
@@ -0,0 +1,7 @@
+const card = document.querySelector('.card-login');
+const start = document.querySelector('.btn-start');
+
+start.addEventListener('click', () => {
+  card.classList.add('display');
+});
+
