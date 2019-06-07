ffe3fe1af5ef6f51423933478a8291f291f9fa41
Guillaume Casile <g.casile@gmail.com>
2019-06-07 11:36:29 +0200

    Remove button on search

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -4,11 +4,11 @@ function autocomplete() {
     if (flatAddresses.length > 0) {
       flatAddresses.forEach((flatAddress) => {
         const autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
-        google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
-          if (e.key === "Enter") {
-            e.preventDefault(); // Do not submit the form on Enter.
-          }
-        });
+        // google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
+        //   if (e.key === "Enter") {
+        //     e.preventDefault(); // Do not submit the form on Enter.
+        //   }
+        // });
       });
     }
   });
