5c05b53d07678794becdd97bb3fdf9b49bb44aff
Frederic Laffont <frederic_laffont@msn.com>
2019-06-07 15:57:52 +0200

    add pawk now feature

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,12 @@
 class SpotsController < ApplicationController
   def create
+    @spot = Spot.new ()
+
+    @spot.lng = params[:spot][:lng]
+    @spot.lat = params[:spot][:lat]
+    
+    @spot.save
+      
   end
 
   def update
