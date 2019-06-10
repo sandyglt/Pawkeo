fa85eccf10e71d4c119b4245c20882eed567ba1e
Myriam Chahlafi <myriam.chah@gmail.com>
2019-06-10 10:45:30 +0200

    Small front fixes

--- app/javascript/components/signin.js
+++ app/javascript/components/signin.js
@@ -1,6 +1,7 @@
 const card = document.querySelector('.card-login');
 const start = document.querySelector('.btn-start');
 const leave = document.getElementById('cross-leave');
+const leaveup = document.getElementById('cross-leave-signup');
 
 if (start) {
   start.addEventListener('click', () => {
@@ -11,5 +12,11 @@ if (start) {
 if (leave) {
   leave.addEventListener('click', () => {
     card.classList.remove('display');
+  });
+}
+
+if (leaveup) {
+  leaveup.addEventListener('click', () => {
+    location.href = 'https://www.pawkeo.com';
   });
 }
